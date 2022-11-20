43148f3a3673e443d14f665a1926ceb12413c10d
luiz-henrique-chaves <88977217+luiz-henrique-chaves@users.noreply.github.com>
2022-11-20 10:27:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# list-repositories-next-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/list-repositories-next-js)
---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import React, { useState, useEffect } from 'react';
+
+export default function Home() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    fetch('https://api.github.com/users/luiz-henrique-chaves/repos')
+      .then((response) => response.json())
+      .then((data) => {
+        const repositoriesNames = data.map((item) => item);
+        setRepositories(repositoriesNames);
+      });
+  }, []);
+
+  return (
+    <div className={styles.row}>
+      <div className={styles.column}>
+        <h3>Github</h3>
+        <p>
+          <mark className={styles.subtitulogit}>@luiz-henrique-chaves</mark>
+        </p>
+        <ul>
+          {repositories.map((repo) => (
+            <li key={repo}>{repo.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.column}>Guichê</div>
+    </div>
+  );
+}
